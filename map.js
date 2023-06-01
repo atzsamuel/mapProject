b0903f38d891daa40fbde8b2d854069c158d5fe4
JAlexanderSS <jalexanderss5003@gmail.com>
2023-06-01 16:59:40 -0600

    Pintar rutas alternativas

--- map.js
+++ map.js
@@ -2,35 +2,69 @@ function initMap() {
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 7,
-        center: { lat: 41.85, lng: -87.65 },
+      zoom: 15,
+      center: { lat: 14.673373964347673, lng: -90.80445764985082 },
     });
-
+  
     directionsRenderer.setMap(map);
-
+  
     const onChangeHandler = function () {
-        calculateAndDisplayRoute(directionsService, directionsRenderer);
+      calculateAndDisplayRoute(directionsService, directionsRenderer);
     };
-
+  
     document.getElementById("start").addEventListener("change", onChangeHandler);
     document.getElementById("end").addEventListener("change", onChangeHandler);
-}
-
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  }
+  
+  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     directionsService
-        .route({
-            origin: {
-                query: document.getElementById("start").value,
-            },
-            destination: {
-                query: document.getElementById("end").value,
-            },
-            travelMode: google.maps.TravelMode.DRIVING,
-        })
-        .then((response) => {
-            directionsRenderer.setDirections(response);
-        })
-        .catch((e) => window.alert("Directions request failed due to " + status));
-}
-
-window.initMap = initMap;
+      .route({
+        origin: {
+          query: document.getElementById("start").value,
+        },
+        destination: {
+          query: document.getElementById("end").value,
+        },
+        travelMode: google.maps.TravelMode.DRIVING,
+        provideRouteAlternatives: true, // Habilita las rutas alternativas
+      })
+      .then((response) => { 
+        directionsRenderer.setDirections(response);
+        // Muestra todas las rutas obtenidas en el mapa
+        directionsRenderer.setRouteIndex(0);
+        directionsRenderer.setOptions({
+          suppressMarkers: true,
+        });
+        directionsRenderer.setOptions({
+          preserveViewport: true,
+          polylineOptions: {
+            strokeWeight: 6,
+            strokeOpacity: 0.7,
+            strokeColor: "blue",
+          },
+        });
+  
+        if (response.routes && response.routes.length > 1) {
+          for (let i = 1; i < response.routes.length; i++) {
+            const altRenderer = new google.maps.DirectionsRenderer();
+            altRenderer.setOptions({
+              suppressMarkers: true,
+              preserveViewport: true,
+              polylineOptions: {
+                strokeWeight: 4,
+                strokeOpacity: 0.5,
+                strokeColor: "gray",
+              },
+            });
+            altRenderer.setMap(directionsRenderer.getMap());
+            altRenderer.setDirections(response);
+            altRenderer.setRouteIndex(i);
+          }
+        }
+      })
+      .catch((e) =>
+        window.alert("Directions request failed due to " + e.status)
+      );
+  }
+  
+  window.initMap = initMap;
